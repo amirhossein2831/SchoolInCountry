e292e1688e55d2af4b8f5486f7d94c01e48523b8
Amirhossein <Amirmemool12@gmail.com>
2023-09-19 13:59:26 +0330

    :hammer: chore: defind route for the country

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\V1\CountryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => 'v1'], function () {
-
+Route::group(['prefix' => 'V1'], function () {
+        Route::apiResource('country', CountryController::class);
 });
