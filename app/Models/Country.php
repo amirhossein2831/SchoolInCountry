20c0f0fabce428be2bf77c771907d909f1b20a87
Amirhossein <Amirmemool12@gmail.com>
2023-09-20 15:50:50 +0330

    :card_file_box: DB: implement the relation between the country and state

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Country extends Model
 {
@@ -21,6 +22,11 @@ class Country extends Model
     public function continent(): BelongsTo
     {
         return $this->belongsTo(Continent::class);
+    }
+
+    public function states(): HasMany
+    {
+        return $this->hasMany(State::class);
     }
 
     protected static function boot(): void
