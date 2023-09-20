20c6fd2cf55afb845c2478bf1dd4d28deaf0be83
Amirhossein <Amirmemool12@gmail.com>
2023-09-20 11:21:23 +0330

    :hammer: chore: define the rule for create country

--- app/Http/Requests/V1/Country/StoreCountryRequest.php
+++ app/Http/Requests/V1/Country/StoreCountryRequest.php
@@ -11,9 +11,9 @@ class StoreCountryRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -21,10 +21,24 @@ class StoreCountryRequest extends FormRequest
      *
      * @return array<string, mixed>
      */
-    public function rules()
+    public function rules(): array
     {
         return [
-            //
+            'name'=>'required|string',
+            'capital'=>'required|string',
+            'language'=>'required|string',
+            'currencyName'=>'required|string',
+            'continentId'=>'required|numeric'
         ];
+    }
+
+    public function prepareForValidation(): void
+    {
+        $this->merge(
+            [
+                'currency_name'=>$this->currencyName,
+                'continent_id' => $this->continentId
+            ]
+        );
     }
 }
