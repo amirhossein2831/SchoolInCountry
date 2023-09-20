b44541e8f0d52a0c74fce85a56ab664487578c5f
Amirhossein <Amirmemool12@gmail.com>
2023-09-20 16:26:37 +0330

    :recycle: refactor: change search method in filter

--- app/Http/Filter/V1/Filter.php
+++ app/Http/Filter/V1/Filter.php
@@ -2,27 +2,34 @@
 
 namespace App\Http\Filter\V1;
 
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+
 class Filter
 {
-    public function search($builder,$value,$column)
-    {
-        return $value
-            ? $builder->where($column, 'LIKE', "%$value%")
-            : $builder;
-    }
-
-    public function all($builder, $value,$query)
-    {
-        return $value
-            ? $builder->get()
-            : $builder->paginate()->appends($query);
-    }
-
-    public function relation($builder,$value,array $relation)
+    public function relation(Builder $builder,$value,array $relation): Builder
     {
 
         return $value
             ? $builder->with($relation)
             : $builder;
+    }
+
+    public function search(Builder $builder,$value,$columns): Builder
+    {
+        if ($value) {
+            foreach ($columns as $column) {
+                $builder->orWhere($column, 'LIKE', "%$value%");
+            }
+        }
+        return $builder;
+    }
+
+    public function all(Builder $builder, $value,$query): Collection|LengthAwarePaginator|array
+    {
+        return $value
+            ? $builder->get()
+            : $builder->paginate()->appends($query);
     }
 }
